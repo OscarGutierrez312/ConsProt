f6b3df739d8a9a4af6734befb8aa8ffa4bcbfba7
OscarGutierrez312 <oscargutierrez1102@gmail.com>
2023-01-12 10:01:53 -0500

    fix

--- ang-app/Front/src/app/agenda/agenda.component.ts
+++ ang-app/Front/src/app/agenda/agenda.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AgendaComponent {
     selectedDate!: Date | null;
+    value = '';
+    horas=[
+      {desc:"12"}, {desc:"1"}, {desc:"2"},
+      {desc:"3"}, {desc:"4"}, {desc:"5"},
+      {desc:"6"}, {desc:"7"}, {desc:"8"},
+      {desc:"9"}, {desc:"10"}, {desc:"11"}
+    ];
+    mins=[
+      {desc:":10"}, {desc:":15"}, {desc:":20"},
+      {desc:":25"}, {desc:":30"}, {desc:":35"},
+      {desc:":40"}, {desc:":45"}, {desc:":50"}
+    ];
+    time=[
+      {desc:"AM"}, {desc:"PM"}
+    ];
 }
